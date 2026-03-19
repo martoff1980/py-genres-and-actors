b1e907e6bdc08b3d5e7fdc64597081c7ed444cab
martoff <martoff@ukr.net>
2026-03-19 15:11:34 +0200

    'Solution'

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
 import init_django_orm  # noqa: F401
+import django
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
+
+django.setup()
 
 
 def main() -> QuerySet:
-    pass
+
+    # 1. CREATE
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    # 2. UPDATE
+    dramma_genre = Genre.objects.get(name="Dramma")
+    dramma_genre.name = "Drama"
+    dramma_genre.save()
+
+    george = Actor.objects.get(first_name="George", last_name="Klooney")
+    george.last_name = "Clooney"
+    george.save()
+
+    kianu = Actor.objects.get(first_name="Kianu", last_name="Reaves")
+    kianu.first_name = "Keanu"
+    kianu.last_name = "Reeves"
+    kianu.save()
+
+    # 3. DELETE
+    action_genre = Genre.objects.get(name="Action")
+    action_genre.delete()
+
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # 4. RETURN
+    smith_actors = Actor.objects.filter(
+        last_name="Smith"
+    ).order_by(
+        "first_name"
+    )
+    return smith_actors
